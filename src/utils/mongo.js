29f63f7c99c90500c7c4cc584e38f8d6e1e6dea5
ibrahim <ibrahim@ibrahim-MacBook-Pro.local>
2021-10-30 23:39:23 +0300

    connection string changed.

--- src/utils/mongo.js
+++ src/utils/mongo.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require('mongodb');
 
 const getClientObject = () => {
-  const connectionString = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@challenge-xzwqd.mongodb.net/${process.env.DB_NAME}?retryWrites=true`;
+  const connectionString = process.env.MONGO_CONNECTIONSTRING;
 
   return MongoClient.connect(connectionString, { useNewUrlParser: true });
 };
